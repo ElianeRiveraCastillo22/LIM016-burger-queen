27459232878c9e08295409ea7a9486ce3e37c6dc
ElianeRiveraCastillo22 <elianercast@gmail.com>
2022-04-21 18:09:36 -0500

    Se revirtieron los cambios

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,6 +11,7 @@ function Login() {
             <button className="BtnLogin">
               Salón
             </button>
+
           </Link>
           <Link to="/cocina" className="BoxButton">
             <button className="BtnLogin" >Cocina</button>
